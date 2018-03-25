26f5a27a2a871f9a8deae6e9d58e27be1fe6880d
Peter B Smith <peter@upstate.agency>
2018-03-25 16:09:24 -0400

    feat(component): add base components

---
+++ src/app/components/episode/episode.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wire-episode',
+  templateUrl: './episode.component.html',
+  styleUrls: ['./episode.component.css']
+})
+export class EpisodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
