767985df9b5f783dde863fa797eb68c20a07c05c
Peter B Smith <peter@upstate.agency>
2018-03-25 15:59:10 -0400

    feat(constant): add base constants

---
+++ src/app/constants/episodes.ts
@@ -0,0 +1,155 @@
+import { Episode } from '../models/episode';
+
+export const EPISODES: Episode[] = [
+  {
+    title: 'The Target',
+    scenes: [
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      }
+    ]
+  },
+  {
+    title: 'The Detail',
+    scenes: [
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      },
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      },
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      }
+    ]
+  },
+  {
+    title: 'The Buys',
+    scenes: [
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      }
+    ]
+  },
+  {
+    title: 'Old Cases',
+    scenes: [
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      },
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      },
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      }
+    ]
+  },
+  {
+    title: 'The Pager',
+    scenes: [
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      }
+    ]
+  },
+  {
+    title: 'The Wire',
+    scenes: [
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      }
+    ]
+  },
+  {
+    title: 'One Arrest',
+    scenes: [
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      }
+    ]
+  },
+  {
+    title: 'Lessons',
+    scenes: [
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      }
+    ]
+  },
+  {
+    title: 'Game Day',
+    scenes: [
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      }
+    ]
+  },
+  {
+    title: 'The Cost',
+    scenes: [
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      }
+    ]
+  },
+  {
+    title: 'The Hunt',
+    scenes: [
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      }
+    ]
+  },
+  {
+    title: 'Cleaning Up',
+    scenes: [
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+      }
+    ]
+  },
+  {
+    title: 'Sentencing',
+    scenes: [
+      {
+        transcript: '',
+        video: '',
+        analysis: '',
+        intro: ''
+      }
+    ]
+  },
+];
---
+++ src/app/constants/terms.ts
@@ -0,0 +1,10 @@
+import { Term } from '../models/term';
+
+export const TERMS: {
+  [word: string]: Term;
+} = {
+  'stance-theory': {
+    word: 'Stance Theory',
+    definition: ''
+  }
+};
