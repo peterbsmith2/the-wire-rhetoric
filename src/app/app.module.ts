a2fba94409ad0589fb885b0f3af780abff216130
Peter B Smith <peter@upstate.agency>
2018-03-25 16:09:38 -0400

    feat(app): include components and services in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,24 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import { EpisodeComponent } from './components/episode/episode.component';
+import { SceneComponent } from './components/scene/scene.component';
+
+import { EpisodeService } from './services/episode.service';
+import { TermService } from './services/term.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EpisodeComponent,
+    SceneComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [EpisodeService, TermService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
